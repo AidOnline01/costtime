0667f3d1a9384f3835e8cb327463b9a1c2898754
ITDevv <aidonline01@gmail.com>
2021-11-14 19:49:58 +0000

    float hours and mins allowed

--- src/entries/PriceList.js
+++ src/entries/PriceList.js
@@ -1,31 +1,29 @@
 import { useState } from 'react';
 import declinationOfNum from '@/utils/declinationOfNum';
 
-const price = 20;
-const roundTo = 1;
-const currency = '$';
-
 export default function PriceList(props) {
   const [text, setText] = useState('');
   const [price, setPrice] = useState(500);
   const [roundTo, setRoundTo] = useState(50);
   const [currency, setCurrency] = useState('грн');
+  const [multiplier, setMultiplier] = useState(2.2);
   let totalPrice = 0;
   let totalHours = 0;
   let totalDays = 0;
+  let weekends = 0;
 
   const convertTimeToPrice = () => {
     // hours + mins
-    const matches = [...text.matchAll(/((\d+)hour\s?)?((\d+)min)?/g)];
+    const matches = [...text.matchAll(/(([\d\.]+)hour\s?)?((\d+)min)?/g)];
     let result = text;
     for(let match of matches) {
-      const hours = +match[2] || 0;
-      const mins = +match[4] || 0;
+      const hours = parseFloat(match[2]) || 0;
+      const mins = parseFloat(match[4]) || 0;
+      console.log(hours);
       if(!hours && !mins) continue;
 
       const matchHours = hours + mins / 60;
-      const matchPrice = Math.ceil(price * matchHours / roundTo) * roundTo;
-      totalPrice += matchPrice;
+      const matchPrice = Math.ceil(price * matchHours / roundTo * multiplier) * roundTo;
       totalHours += matchHours;
 
       result = result.replace(match[0], matchPrice + currency);
@@ -33,7 +31,10 @@ export default function PriceList(props) {
       result = result.replace(hours + 'hours' + ' ' + mins + 'mins', matchHours);
     }
 
-    totalDays = Math.ceil(totalHours / 4);
+    totalDays = Math.ceil(totalHours * multiplier / 4);
+    totalPrice = Math.ceil(totalHours * price * multiplier / roundTo) * roundTo;
+    weekends = Math.round(totalDays / 7 * 2);
+
 
     return result;
   };
@@ -42,7 +43,7 @@ export default function PriceList(props) {
     <div className="price-list__top">
       <div className="price-list__field">
         <label>Цена</label>
-        <input placeholder="Цена" value={price} onChange={e => setPrice(+e.target.value)} />
+        <input placeholder="Цена" value={price} onChange={e => setPrice(e.target.value)} />
       </div>
 
       <div className="price-list__field">
@@ -52,12 +53,17 @@ export default function PriceList(props) {
 
       <div className="price-list__field">
         <label>Округлять до</label>
-        <input placeholder="Округлять до" value={roundTo} onChange={e => setRoundTo(+e.target.value)} />
+        <input placeholder="Округлять до" value={roundTo} onChange={e => setRoundTo(e.target.value)} />
+      </div>
+
+      <div className="price-list__field">
+        <label>Мультипликатор</label>
+        <input placeholder="Мультипликатор" type="text" value={multiplier} onChange={e => setMultiplier(e.target.value)} />
       </div>
     </div>
 
     <div className="price-list__left">
-      <textarea value={text} onChange={e => setText(e.target.value)}></textarea>
+      <textarea value={text} type="text" onChange={e => setText(e.target.value)}></textarea>
     </div>
 
     <div className="price-list__right">
@@ -65,7 +71,9 @@ export default function PriceList(props) {
     </div>
 
     <div className="price-list__total">
-      <b>Итого:</b> {totalPrice}{currency}, <b>Сроки:</b> {totalDays * 2} {declinationOfNum(totalDays * 2, ['день', 'дня', 'дней'])}
+      <b>Итого:</b> {totalPrice}{currency}, 
+      <b> Сроки:</b> {totalDays} {declinationOfNum(totalDays, ['день', 'дня', 'дней'])}, 
+      <b> Выходные:</b> {weekends} {declinationOfNum(weekends, ['день', 'дня', 'дней'])}
     </div>
   </div>;
 }
